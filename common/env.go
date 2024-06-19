fafa61f9e64f4e7ea4239c4b6930f370c2d81479
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2024-06-19 11:19:08 +0700

    feat: 🎸 initialize dev container
    create base docker container for each services

---
+++ common/env.go
@@ -0,0 +1,14 @@
+package common
+
+import (
+	"syscall"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+ 
+func GetEnv(name, defaultValue string) string { 
+	if value, ok := syscall.Getenv(name); ok {
+		return value
+	}
+	return defaultValue
+}
---
+++ gateway/go.mod
@@ -0,0 +1,4 @@
+module github.com/dot-coaching/gateway
+
+go 1.22.4
+

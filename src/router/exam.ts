1237cc8b13125df76d978441c501255189b0310a
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2024-06-23 13:50:50 +0700

    feat: 🎸 implement exam bussiness logic
    implement basic CRUD of exam model

--- src/router/exam.ts
+++ src/router/exam.ts
@@ -1,11 +1,97 @@
-import { InsertExamSchema } from "~/schemas/exam";
+import { eq, gt } from "drizzle-orm";
+import { db } from "~/db";
+import { exams, InsertExamSchema } from "~/schemas/exam";
+import { APIResponse } from "~/types";
 import { ServerType } from "..";
 
 export function createExamRouter(app: ServerType) {
-  app.get("/", ({ query: { cursor, limit } }) => {});
-  app.get("/:id", ({ params: { id } }) => {});
-  app.post("/", () => {}, { body: InsertExamSchema });
-  app.put("/:id", ({ params: { id } }) => {}, { body: InsertExamSchema });
-  app.delete("/:id", ({ params: { id } }) => {});
+  app.get("/", async ({ query: { cursor, limit } }) => {
+    const res = await db
+      .select()
+      .from(exams)
+      .where(gt(exams.id, parseInt(cursor as string) || 0))
+      .limit(parseInt(limit as string) || 10);
+
+    if (res.length == 0) {
+      return {
+        error: "No exams found",
+      } satisfies APIResponse;
+    }
+    return {
+      message: "Exams found",
+      data: res,
+    } satisfies APIResponse;
+  });
+  app.get("/:id", async ({ params: { id } }) => {
+    const res = await db
+      .select()
+      .from(exams)
+      .where(eq(exams.id, parseInt(id)))
+      .limit(1);
+
+    if (res.length == 0) {
+      return {
+        error: `Exam with id ${id} not found`,
+      } satisfies APIResponse;
+    }
+
+    return {
+      message: `Exam with id ${id} found`,
+      data: res[0],
+    } satisfies APIResponse;
+  });
+  app.post(
+    "/",
+    async ({ body }) => {
+      const res = await db.insert(exams).values(body).returning();
+      if (res.length == 0) {
+        return {
+          error: "Failed to insert exam",
+        } satisfies APIResponse;
+      }
+
+      return {
+        message: "Exam inserted",
+        data: res[0],
+      } satisfies APIResponse;
+    },
+    {
+      body: InsertExamSchema,
+    }
+  );
+  app.put(
+    "/:id",
+    async ({ params: { id }, body }) => {
+      const res = await db
+        .update(exams)
+        .set(body)
+        .where(eq(exams.id, parseInt(id)))
+        .returning();
+
+      if (res.length == 0) {
+        return {
+          error: `Failed to update exam with id ${id}`,
+        } satisfies APIResponse;
+      }
+    },
+    { body: InsertExamSchema }
+  );
+  app.delete("/:id", async ({ params: { id } }) => {
+    const res = await db
+      .delete(exams)
+      .where(eq(exams.id, parseInt(id)))
+      .returning();
+
+    if (res.length == 0) {
+      return {
+        error: `Failed to delete exam with id ${id}`,
+      } satisfies APIResponse;
+    }
+
+    return {
+      message: `Exam with id ${id} deleted`,
+      data: res[0],
+    } satisfies APIResponse;
+  });
   return app;
 }

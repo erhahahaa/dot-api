8f28bede5a51c0d823f0e5081468d35e1ecedd04
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2024-06-23 13:51:55 +0700

    feat: 🎸 implement tactical bussiness logic
    implement basic CRUD of tactical model

--- src/router/tactical.ts
+++ src/router/tactical.ts
@@ -1,11 +1,97 @@
-import { InsertTacticalSchema } from "~/schemas/tacticals";
+import { eq, gt } from "drizzle-orm";
+import { db } from "~/db";
+import { InsertTacticalSchema, tacticals } from "~/schemas/tacticals";
+import { APIResponse } from "~/types";
 import { ServerType } from "..";
 
 export function createTacticalRouter(app: ServerType) {
-  app.get("/", ({ query: { cursor, limit } }) => {});
-  app.get("/:id", ({ params: { id } }) => {});
-  app.post("/", () => {}, { body: InsertTacticalSchema });
-  app.put("/:id", ({ params: { id } }) => {}, { body: InsertTacticalSchema });
-  app.delete("/:id", ({ params: { id } }) => {});
+  app.get("/", async ({ query: { cursor, limit } }) => {
+    const res = await db
+      .select()
+      .from(tacticals)
+      .where(gt(tacticals.id, parseInt(cursor as string) || 0))
+      .limit(parseInt(limit as string) || 10);
+
+    if (res.length == 0) {
+      return {
+        error: "No tacticals found",
+      } satisfies APIResponse;
+    }
+    return {
+      message: "Tacticals found",
+      data: res,
+    } satisfies APIResponse;
+  });
+  app.get("/:id", async ({ params: { id } }) => {
+    const res = await db
+      .select()
+      .from(tacticals)
+      .where(eq(tacticals.id, parseInt(id)))
+      .limit(1);
+
+    if (res.length == 0) {
+      return {
+        error: `Tactical with id ${id} not found`,
+      } satisfies APIResponse;
+    }
+
+    return {
+      message: `Tactical with id ${id} found`,
+      data: res[0],
+    } satisfies APIResponse;
+  });
+  app.post(
+    "/",
+    async ({ body }) => {
+      const res = await db.insert(tacticals).values(body).returning();
+      if (res.length == 0) {
+        return {
+          error: "Failed to insert tactical",
+        } satisfies APIResponse;
+      }
+
+      return {
+        message: "Tactical inserted",
+        data: res[0],
+      } satisfies APIResponse;
+    },
+    {
+      body: InsertTacticalSchema,
+    }
+  );
+  app.put(
+    "/:id",
+    async ({ params: { id }, body }) => {
+      const res = await db
+        .update(tacticals)
+        .set(body)
+        .where(eq(tacticals.id, parseInt(id)))
+        .returning();
+
+      if (res.length == 0) {
+        return {
+          error: `Failed to update tactical with id ${id}`,
+        } satisfies APIResponse;
+      }
+    },
+    { body: InsertTacticalSchema }
+  );
+  app.delete("/:id", async ({ params: { id } }) => {
+    const res = await db
+      .delete(tacticals)
+      .where(eq(tacticals.id, parseInt(id)))
+      .returning();
+
+    if (res.length == 0) {
+      return {
+        error: `Failed to delete tactical with id ${id}`,
+      } satisfies APIResponse;
+    }
+
+    return {
+      message: `Tactical with id ${id} deleted`,
+      data: res[0],
+    } satisfies APIResponse;
+  });
   return app;
 }

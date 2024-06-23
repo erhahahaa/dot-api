639d4258f735467f85fb7b1c7472e666b4230303
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2024-06-23 11:56:05 +0700

    feat: 🎸 add health response
    create health endpoint to monitor server

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,13 @@
 import { ServerType } from "..";
 import { createAuthRouter } from "./auth";
 import { createExamRouter } from "./exam";
+import { createHealthRouter } from "./health";
 import { createProgramRouter } from "./program";
 import { createQuestionRouter } from "./question";
 import { createTacticalRouter } from "./tactical";
 
 export function createRouter(app: ServerType) {
-  app.group("/api", (app) => {
+  return app.group("/api", (app) => {
     app.group("/auth", (app) => createAuthRouter(app as any));
     app.group("/program", (app) => {
       createProgramRouter(app as any);
@@ -18,8 +19,7 @@ export function createRouter(app: ServerType) {
       app.group("/tactical", (app) => createTacticalRouter(app as any));
       return app;
     });
+    app.group("/health", (app) => createHealthRouter(app as any));
     return app;
   });
-
-  return app;
 }
